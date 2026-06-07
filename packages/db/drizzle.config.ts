dfa86dffb9d97e55a756862290a3339f41c2461e
Sanket4450 <talaviyasanket01@gmail.com>
2026-06-07 11:12:52 +0530

    throw error in case DB URL is not defined into drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,14 +4,20 @@ import dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
 dotenv.config({
-  path: path.resolve(process.cwd(), 'apps/api/.env'),
+  path: path.resolve(process.cwd(), '../../apps/api/.env'),
 });
+
+const dbUrl = process.env.DB_URL;
+
+if (!dbUrl) {
+  throw new Error('DB_URL environment variable is not defined');
+}
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/schema/**/*',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DB_URL!,
+    url: dbUrl,
   },
 });
